01f59e191ef38e68b6082d6145dfc3f6a25ae5ee
HP <HP@DESKTOP-TI041JI>
2024-08-22 04:43:12 -1100

    Add project files.

---
+++ CarSimulation/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace CarSimulation
+{
+    partial class CarSimulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CarSimulation));
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.BtnBrake = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Car1 = new System.Windows.Forms.PictureBox();
+            this.Car2 = new System.Windows.Forms.PictureBox();
+            this.Car3 = new System.Windows.Forms.PictureBox();
+            this.Car4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.BackColor = System.Drawing.Color.MistyRose;
+            this.BtnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnPlay.ForeColor = System.Drawing.Color.Black;
+            this.BtnPlay.Location = new System.Drawing.Point(169, 447);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(68, 23);
+            this.BtnPlay.TabIndex = 1;
+            this.BtnPlay.Text = "PLAY";
+            this.BtnPlay.UseVisualStyleBackColor = false;
+            this.BtnPlay.Click += new System.EventHandler(this.PlayBTN);
+            // 
+            // BtnBrake
+            // 
+            this.BtnBrake.BackColor = System.Drawing.Color.MistyRose;
+            this.BtnBrake.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnBrake.ForeColor = System.Drawing.Color.Black;
+            this.BtnBrake.Location = new System.Drawing.Point(250, 447);
+            this.BtnBrake.Name = "BtnBrake";
+            this.BtnBrake.Size = new System.Drawing.Size(68, 23);
+            this.BtnBrake.TabIndex = 1;
+            this.BtnBrake.Text = "BRAKE";
+            this.BtnBrake.UseVisualStyleBackColor = false;
+            this.BtnBrake.Click += new System.EventHandler(this.BrakeBTN);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MistyRose;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(659, 447);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "LAUNCH";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.LaunchBTN);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MistyRose;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(333, 447);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "GEAR1";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.GEAR1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MistyRose;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(414, 447);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(68, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "GEAR2";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.GEAR2);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.MistyRose;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(576, 447);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(68, 23);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "GEAR4";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.GEAR4);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(495, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GEAR3";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.GEAR3);
+            // 
+            // Car1
+            // 
+            this.Car1.Image = ((System.Drawing.Image)(resources.GetObject("Car1.Image")));
+            this.Car1.Location = new System.Drawing.Point(4, 328);
+            this.Car1.Name = "Car1";
+            this.Car1.Size = new System.Drawing.Size(115, 90);
+            this.Car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car1.TabIndex = 0;
+            this.Car1.TabStop = false;
+            this.Car1.Click += new System.EventHandler(this.Car1_Click);
+            // 
+            // Car2
+            // 
+            this.Car2.Image = ((System.Drawing.Image)(resources.GetObject("Car2.Image")));
+            this.Car2.Location = new System.Drawing.Point(784, 328);
+            this.Car2.Name = "Car2";
+            this.Car2.Size = new System.Drawing.Size(90, 90);
+            this.Car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car2.TabIndex = 0;
+            this.Car2.TabStop = false;
+            this.Car2.Click += new System.EventHandler(this.Car2_Click);
+            // 
+            // Car3
+            // 
+            this.Car3.Image = ((System.Drawing.Image)(resources.GetObject("Car3.Image")));
+            this.Car3.Location = new System.Drawing.Point(784, 2);
+            this.Car3.Name = "Car3";
+            this.Car3.Size = new System.Drawing.Size(90, 90);
+            this.Car3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car3.TabIndex = 0;
+            this.Car3.TabStop = false;
+            // 
+            // Car4
+            // 
+            this.Car4.Image = ((System.Drawing.Image)(resources.GetObject("Car4.Image")));
+            this.Car4.Location = new System.Drawing.Point(4, 12);
+            this.Car4.Name = "Car4";
+            this.Car4.Size = new System.Drawing.Size(98, 90);
+            this.Car4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car4.TabIndex = 0;
+            this.Car4.TabStop = false;
+            this.Car4.Click += new System.EventHandler(this.Car4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // CarSimulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(875, 482);
+            this.Controls.Add(this.Car3);
+            this.Controls.Add(this.Car4);
+            this.Controls.Add(this.Car2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Car1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnBrake);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "CarSimulation";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CarSimulation";
+            ((System.ComponentModel.ISupportInitialize)(this.Car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Button BtnBrake;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Car3;
+        private System.Windows.Forms.PictureBox Car2;
+        private System.Windows.Forms.PictureBox Car1;
+        private System.Windows.Forms.PictureBox Car4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ CarSimulation/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarSimulation
+{
+    public partial class CarSimulation : Form
+    {
+        int Speed = 15;
+        public CarSimulation()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBTN(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Car2.Visible = false;
+            Car3.Visible = false;
+            Car4.Visible = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Car1.Location = new Point(Car1.Location.X + Speed, Car1.Location.Y);
+
+            if(Car1.Location.X > 784)
+            {
+                Car1.Visible = false;
+                Car2.Visible = true;
+                timer2.Start();
+                timer1.Stop();
+                Car1.Location = new Point(3, 328);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Car2.Location = new Point(Car2.Location.X, Car2.Location.Y - Speed);
+
+            if (Car2.Location.Y < 2)
+            {
+                Car2.Visible = false;
+                Car3.Visible = true;
+                timer3.Start();
+                timer2.Stop();
+                Car2.Location = new Point(784, 328);
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Car3.Location = new Point(Car3.Location.X - Speed, Car3.Location.Y);
+
+            if (Car3.Location.X < 4)
+            {
+                Car3.Visible = false;
+                Car4.Visible = true;
+                timer4.Start();
+                timer3.Stop();
+                Car3.Location = new Point(784, 2);
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Car4.Location = new Point(Car4.Location.X, Car4.Location.Y + Speed);
+
+            if (Car4.Location.Y > 328)
+            {
+                Car4.Visible = false;
+                Car1.Visible = true;
+                timer1.Start();
+                timer4.Stop();
+                Car4.Location = new Point(4, 2);
+            }
+        }
+
+        private void BrakeBTN(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+        }
+
+        private void GEAR1(object sender, EventArgs e)
+        {
+            Speed = 25;
+        }
+
+        private void GEAR2(object sender, EventArgs e)
+        {
+            Speed = 35;
+        }
+
+        private void GEAR3(object sender, EventArgs e)
+        {
+            Speed = 45;
+        }
+
+        private void GEAR4(object sender, EventArgs e)
+        {
+            Speed = 55;
+        }
+
+        private void LaunchBTN(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            Car1.Visible = true;
+            Car2.Visible = false;
+            Car3.Visible = false;
+            Car4.Visible = false;
+            Car1.Location = new Point(3, 328);
+        }
+
+        private void Car4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Car2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Car1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
